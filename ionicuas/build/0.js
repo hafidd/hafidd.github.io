4beb9c574fd7eca54b17e4d1fffa04c5e5742e9b
hafidd <hafid.f.septiawan@gmail.com>
2018-07-30 23:30:04 +0700

    responsi ionic

---
+++ ionicuas/build/0.js
@@ -0,0 +1,151 @@
+webpackJsonp([0],{
+
+/***/ 274:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "DetailPageModule", function() { return DetailPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__detail__ = __webpack_require__(275);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var DetailPageModule = /** @class */ (function () {
+    function DetailPageModule() {
+    }
+    DetailPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__detail__["a" /* DetailPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__detail__["a" /* DetailPage */]),
+            ],
+        })
+    ], DetailPageModule);
+    return DetailPageModule;
+}());
+
+//# sourceMappingURL=detail.module.js.map
+
+/***/ }),
+
+/***/ 275:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DetailPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common_http__ = __webpack_require__(99);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+/**
+ * Generated class for the DetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var DetailPage = /** @class */ (function () {
+    //private url = "http://192.168.43.65/ionicuas/web/";
+    function DetailPage(navCtrl, http, navParams) {
+        this.navCtrl = navCtrl;
+        this.http = http;
+        this.navParams = navParams;
+        this.images = [];
+        this.url = "http://localhost:8080/";
+    }
+    DetailPage.prototype.ionViewDidLoad = function () {
+        //console.log(this.navParams.get("record"));
+        if (this.navParams.get("record")) {
+            this.item = this.navParams.get("record");
+            this.id = this.item.wis_id;
+            this.lat = this.item.wis_lat;
+            this.long = this.item.wis_long;
+            this.name = this.item.wis_nama;
+            this.ket = this.item.wis_keterangan;
+            this.kab = this.item.kab_nama;
+            this.img = this.item.wis_id + '.' + this.item.wis_gambar;
+            this.tampilMap(this.lat, this.long, this.name);
+            this.loadGallery();
+        }
+    };
+    DetailPage.prototype.tampilMap = function (lat, long, name) {
+        var _this = this;
+        var locationOptions = { timeout: 20000, enableHighAccuracy: true };
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var myLatlng = new google.maps.LatLng(lat, long);
+            var options = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            _this.map = new google.maps.Map(document.getElementById('akupeta'), options);
+            _this.buatMarker(name);
+            //document.getElementById('spin').visibility = 'hidden';
+        }, function (error) {
+            console.log(error);
+        }, locationOptions);
+    };
+    DetailPage.prototype.buatMarker = function (name) {
+        var _this = this;
+        var marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: this.map.getCenter()
+        });
+        var konten = "<h5 style='color:rgb(0, 128, 248)'>" + name + "</h5>";
+        var infoWindow = new google.maps.InfoWindow({
+            content: konten,
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(_this.map, marker);
+        });
+    };
+    // load data
+    DetailPage.prototype.loadGallery = function () {
+        var _this = this;
+        this.http
+            .get(this.url + 'wisata-api/get-gallery?id=' + this.id)
+            .subscribe(function (data) {
+            console.dir(data);
+            _this.images = data;
+        }, function (error) {
+            console.dir(error);
+        });
+    };
+    DetailPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-detail',template:/*ion-inline-start:"d:\prg\ionic\ionicuas\src\pages\detail\detail.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      {{name}}\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n<ion-content padding>\n  <ion-row>\n    <ion-col col-12 col-md-8 push-md-4 style="padding-right: 0;">\n      <div id="akupeta" style="width:100%;height:450px;">peta</div>\n    </ion-col>\n    <ion-col col-12 col-md-4 pull-md-8 style="max-height: 450px; overflow: auto;">\n      <div class="">\n        <img src="{{url}}gambar/wisata/{{img}}" style="width: 100%;">\n      </div>\n      <div class="lokasi-judul">\n        <h4>{{name}}</h4>\n      </div>\n      <div class="detail">\n        <p>{{ket}}</p>\n        <hr>\n        <p>\n          <b>{{kab}}, DIY</b>\n        </p>\n        <p>Latitude : {{lat}}</p>\n        <p>Longitude : {{long}}</p>\n      </div>\n    </ion-col>\n  </ion-row>\n  <hr>\n  <ion-row>\n    <ion-col col-12>\n      <h3>Gallery</h3>\n      <ion-row *ngIf="images.length">\n        <ion-col col-6 col-md-3 *ngFor="let data of images">\n          <img src="{{url}}gambar/gallery/{{id}}_{{data.gl_id}}.{{data.gl_gambar}}" style="width: 100%;">\n          <b>{{data.gl_nama}}</b>\n        </ion-col>\n      </ion-row>\n      <ion-row>\n        <ion-col col-12 *ngIf="!images.length">Data gallery tidak ditemukan</ion-col>\n      </ion-row>\n    </ion-col>\n  </ion-row>\n</ion-content>'/*ion-inline-end:"d:\prg\ionic\ionicuas\src\pages\detail\detail.html"*/,
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_common_http__["a" /* HttpClient */],
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]])
+    ], DetailPage);
+    return DetailPage;
+}());
+
+//# sourceMappingURL=detail.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
---
+++ ionicuas/build/main.js
@@ -0,0 +1,265 @@
+webpackJsonp([1],{
+
+/***/ 111:
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = 111;
+
+/***/ }),
+
+/***/ 153:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"../pages/detail/detail.module": [
+		274,
+		0
+	]
+};
+function webpackAsyncContext(req) {
+	var ids = map[req];
+	if(!ids)
+		return Promise.reject(new Error("Cannot find module '" + req + "'."));
+	return __webpack_require__.e(ids[1]).then(function() {
+		return __webpack_require__(ids[0]);
+	});
+};
+webpackAsyncContext.keys = function webpackAsyncContextKeys() {
+	return Object.keys(map);
+};
+webpackAsyncContext.id = 153;
+module.exports = webpackAsyncContext;
+
+/***/ }),
+
+/***/ 197:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomePage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common_http__ = __webpack_require__(99);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var HomePage = /** @class */ (function () {
+    //private url = "http://192.168.43.65/ionicuas/web/";
+    function HomePage(navCtrl, http) {
+        this.navCtrl = navCtrl;
+        this.http = http;
+        this.item = [];
+        this.kabupatens = [];
+        this.jenises = [];
+        this.url = "http://localhost:8080/";
+        this.hideMe = false;
+        this.arrow = "ios-arrow-down";
+        this.jenis = "Semua Wisata";
+        this.kabupaten = "Semua Kabupaten";
+    }
+    HomePage.prototype.ionViewWillEnter = function () {
+        this.load();
+        this.loadSelect();
+        console.dir(this.kabupatens);
+    };
+    // load data
+    HomePage.prototype.load = function () {
+        var _this = this;
+        this.http
+            .get(this.url + 'wisata-api/get-wisata?tipe=' + this.jenis + '&kab=' + this.kabupaten)
+            .subscribe(function (data) {
+            //console.dir(data);
+            _this.item = data;
+        }, function (error) {
+            console.dir(error);
+        });
+    };
+    // load data
+    HomePage.prototype.loadSelect = function () {
+        var _this = this;
+        // kabupaten
+        this.http
+            .get(this.url + 'wisata-api/kabupaten')
+            .subscribe(function (data) {
+            //console.dir(data);
+            _this.kabupatens = data;
+        }, function (error) {
+            console.dir(error);
+        });
+        // jenis
+        this.http
+            .get(this.url + 'wisata-api/jenis')
+            .subscribe(function (data) {
+            //console.dir(data);
+            _this.jenises = data;
+        }, function (error) {
+            console.dir(error);
+        });
+    };
+    // hide
+    HomePage.prototype.hideFilter = function () {
+        this.hideMe = !this.hideMe;
+        this.arrow == "ios-arrow-down" ? this.arrow = "ios-arrow-up" : this.arrow = "ios-arrow-down";
+    };
+    HomePage.prototype.goDetail = function (param) {
+        this.navCtrl.push('DetailPage', param);
+    };
+    HomePage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-home',template:/*ion-inline-start:"d:\prg\ionic\ionicuas\src\pages\home\home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      Wisata Yogyakarta\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n<ion-content>\n  <img src="assets/imgs/yogyakarta.jpeg" alt="yogya" class="imgr" />\n\n  <div>\n    <div>\n      <div text-center>\n        <h6>{{jenis}} di {{kabupaten}}\n          <button ion-button small (click)="hideFilter()">\n            <ion-icon name="{{arrow}}"></ion-icon>\n          </button>\n        </h6>\n      </div>\n      <div *ngIf="hideMe">\n        <ion-row>\n          <ion-col col-12 col-md-4>\n            <ion-item>\n              <ion-label>Kabupaten</ion-label>\n              <ion-select [(ngModel)]="kabupaten">\n                <ion-option value="Semua Kabupaten">Semua Kabupaten</ion-option>\n                <ion-option *ngFor="let kab of kabupatens" value="{{kab.kab_nama}}">{{kab.kab_nama}}</ion-option>\n              </ion-select>\n            </ion-item>\n          </ion-col>\n          <ion-col col-12 col-md-4>\n            <ion-item>\n              <ion-label>Kategori</ion-label>\n              <ion-select [(ngModel)]="jenis">\n                <ion-option value="Semua Wisata">Semua Wisata</ion-option>\n                <ion-option *ngFor="let jns of jenises" value="{{jns.jenis_nama}}">{{jns.jenis_nama}}</ion-option>\n              </ion-select>\n            </ion-item>\n          </ion-col>\n          <ion-col col-12 col-md-1>\n            <button ion-button full (click)="load()">\n              <ion-icon name="search"></ion-icon>\n            </button>\n          </ion-col>\n        </ion-row>\n      </div>\n    </div>\n    <ion-grid>\n      <ion-row>\n        <ion-col col-12 col-md-4 col-lg-3 col-xl-3 *ngFor="let itemData of item">\n          <ion-card>\n            <ion-card-header>\n              <b>{{ itemData.wis_nama }}</b>\n            </ion-card-header>\n            <ion-card-content no-padding>\n              <div (click)="goDetail({record: itemData})" class="img-list">\n                <img src="{{url}}gambar/wisata/{{ itemData.wis_id }}.{{ itemData.wis_gambar }}" alt="{{itemData.wis_nama}}" />\n              </div>\n            </ion-card-content>\n            <ion-row class="cardfooter">\n              <ion-col>\n                <b>\n                  <ion-icon name="md-images"></ion-icon> {{itemData.photos}}\n                </b>\n              </ion-col>\n            </ion-row>\n          </ion-card>\n        </ion-col>\n      </ion-row>\n    </ion-grid>\n\n  </div>\n</ion-content>'/*ion-inline-end:"d:\prg\ionic\ionicuas\src\pages\home\home.html"*/
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_common_http__["a" /* HttpClient */]])
+    ], HomePage);
+    return HomePage;
+}());
+
+//# sourceMappingURL=home.js.map
+
+/***/ }),
+
+/***/ 198:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(199);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(219);
+
+
+Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_1__app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 219:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(30);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(193);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(196);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(273);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(197);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__angular_common_http__ = __webpack_require__(99);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* MyApp */],
+                __WEBPACK_IMPORTED_MODULE_6__pages_home_home__["a" /* HomePage */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_7__angular_common_http__["b" /* HttpClientModule */],
+                __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["c" /* IonicModule */].forRoot(__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* MyApp */], {}, {
+                    links: [
+                        { loadChildren: '../pages/detail/detail.module#DetailPageModule', name: 'DetailPage', segment: 'detail', priority: 'low', defaultHistory: [] }
+                    ]
+                })
+            ],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_2_ionic_angular__["a" /* IonicApp */]],
+            entryComponents: [
+                __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* MyApp */],
+                __WEBPACK_IMPORTED_MODULE_6__pages_home_home__["a" /* HomePage */]
+            ],
+            providers: [
+                __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__["a" /* StatusBar */],
+                __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */],
+                { provide: __WEBPACK_IMPORTED_MODULE_1__angular_core__["u" /* ErrorHandler */], useClass: __WEBPACK_IMPORTED_MODULE_2_ionic_angular__["b" /* IonicErrorHandler */] }
+            ]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 273:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MyApp; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(49);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(196);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(193);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(197);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+
+var MyApp = /** @class */ (function () {
+    function MyApp(platform, statusBar, splashScreen) {
+        this.rootPage = __WEBPACK_IMPORTED_MODULE_4__pages_home_home__["a" /* HomePage */];
+        platform.ready().then(function () {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
+    MyApp = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"d:\prg\ionic\ionicuas\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"d:\prg\ionic\ionicuas\src\app\app.html"*/
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* Platform */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__["a" /* StatusBar */], __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__["a" /* SplashScreen */]])
+    ], MyApp);
+    return MyApp;
+}());
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ })
+
+},[198]);
+//# sourceMappingURL=main.js.map
